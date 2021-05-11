be6abce3c2e50123d51957d6bb1f23d15ac8c1b9
azdad pavilion <azdadanass@gmail.com>
2021-05-11 18:18:07 +0000

    ...

--- src/main/java/ma/azdad/rest/RestClientService.java
+++ src/main/java/ma/azdad/rest/RestClientService.java
@@ -1,21 +1,18 @@
 package ma.azdad.rest;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import ma.azdad.model.Report;
+import ma.azdad.utils.App;
 
 @Component
 public class RestClientService {
 
-	@Value("${myreportsAddress}")
-	private String myreportsAddress;
-
 	public Report getReport(Integer projectId) {
 		try {
 			RestTemplate restTemplate = new RestTemplate();
-			return restTemplate.getForObject(myreportsAddress + "/rest/service/get/report-full/" + projectId, Report.class);
+			return restTemplate.getForObject(App.MYREPORTS.getHttpLink() + "/rest/service/get/report-full/" + projectId, Report.class);
 		} catch (Exception e) {
 			return null;
 		}

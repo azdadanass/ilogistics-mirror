dfb1ee67278fd19b98405305dd374e1d260446ca
Azdad Anass <a.azdad@3gcom-int.com>
2022-10-25 11:00:20 +0200

    view dn --> add button view map (origin/destination)

--- src/main/java/ma/azdad/view/MapView.java
+++ src/main/java/ma/azdad/view/MapView.java
@@ -3,10 +3,19 @@ package ma.azdad.view;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
+import org.primefaces.event.map.OverlaySelectEvent;
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import ma.azdad.model.Site;
+import ma.azdad.service.SiteService;
+import ma.azdad.service.UtilsFunctions;
 
 @ManagedBean
 @Component
@@ -14,38 +23,62 @@ import org.springframework.transaction.annotation.Transactional;
 @Scope("view")
 public class MapView {
 
-	private Double centerLatitude;
-	private Double centerLongitude;
+	@Autowired
+	private SiteService siteService;
 
 	private MapModel mapModel;
-	
-	
+	private Marker marker;
+
+	private Double latitude;
+	private Double longitude;
+	private String markerTitle;
+
+	private Site site;
+	private Integer siteId;
+
+	private String fileName;
+
 	@PostConstruct
-	public void init(){
-		
-	}
-	
+	public void init() {
+		initParameters();
 
-	
+		if (siteId != null && siteService.exists(siteId)) {
+			site = siteService.findOne(siteId);
+			latitude = site.getLatitude();
+			longitude = site.getLongitude();
+			markerTitle = "<b>" + site.getName() + "</b><br/>" + site.getGoogleAddress();
+			if (site.getIsZone())
+				fileName = site.getGeographicFile();
+		}
 
-	
-
-
-	public Double getCenterLatitude() {
-		return centerLatitude;
-	}
-
-	public void setCenterLatitude(Double centerLatitude) {
-		this.centerLatitude = centerLatitude;
+		refreshMapModel();
 	}
 
-	public Double getCenterLongitude() {
-		return centerLongitude;
+	public String getFilePublicUrl() {
+		return "https://sdm.orange.telodigital.com/file/" + fileName;
 	}
 
-	public void setCenterLongitude(Double centerLongitude) {
-		this.centerLongitude = centerLongitude;
+	public void refreshMapModel() {
+		mapModel = new DefaultMapModel();
+		if (latitude != null && longitude != null) {
+			Marker marker = new Marker(new LatLng(latitude, longitude), markerTitle);
+			mapModel.addOverlay(marker);
+		}
+	}
 
+	private void initParameters() {
+		siteId = UtilsFunctions.getIntegerParameter("siteId");
+		latitude = UtilsFunctions.getDoubleParameter("latitude");
+		longitude = UtilsFunctions.getDoubleParameter("longitude");
+		fileName = UtilsFunctions.getParameter("fileName");
+	}
+
+	public void onMarkerSelect(OverlaySelectEvent event) {
+		marker = (Marker) event.getOverlay();
+	}
+
+	public String getCenterValue() {
+		return latitude + "," + longitude;
 	}
 
 	public MapModel getMapModel() {
@@ -54,6 +87,30 @@ public class MapView {
 
 	public void setMapModel(MapModel mapModel) {
 		this.mapModel = mapModel;
+	}
+
+	public Marker getMarker() {
+		return marker;
+	}
+
+	public void setMarker(Marker marker) {
+		this.marker = marker;
+	}
+
+	public Site getSite() {
+		return site;
+	}
+
+	public void setSite(Site site) {
+		this.site = site;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
 
 }

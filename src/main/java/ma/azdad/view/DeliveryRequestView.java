dd2ac2b4284c0fa2362a78285574b9a6d47a3583
Azdad Anass <a.azdad@3gcom-int.com>
2023-05-19 12:14:11 +0100

    canCalculateUnitCost --> change to 0.1%

--- src/main/java/ma/azdad/view/DeliveryRequestView.java
+++ src/main/java/ma/azdad/view/DeliveryRequestView.java
@@ -1309,7 +1309,7 @@ public class DeliveryRequestView extends GenericView<Integer, DeliveryRequest, D
 		return deliveryRequest.getIsInboundNew() //
 				&& DeliveryRequestStatus.DELIVRED.equals(deliveryRequest.getStatus()) //
 						& !deliveryRequest.getBoqMappingList().isEmpty() //
-				&& totalAppLink > 0 && Math.abs(totalAppLink - deliveryRequest.getTotalCost()) / totalAppLink >= 0.01;
+				&& totalAppLink > 0 && Math.abs(totalAppLink - deliveryRequest.getTotalCost()) / totalAppLink >= 0.001;
 	}
 
 	public void calculateUnitCost() {

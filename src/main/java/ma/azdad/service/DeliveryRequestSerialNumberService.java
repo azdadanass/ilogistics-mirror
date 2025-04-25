89e3da7e53ff9eb1abc412a8212d375786f3de94
reda108 <redabouougri12@gmail.com>
2025-04-25 17:51:04 +0100

    Update DeliveryRequestSerialNumberService.java

--- src/main/java/ma/azdad/service/DeliveryRequestSerialNumberService.java
+++ src/main/java/ma/azdad/service/DeliveryRequestSerialNumberService.java
@@ -1,8 +1,12 @@
 package ma.azdad.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.ObjectUtils;
 import org.hibernate.Hibernate;
@@ -18,6 +22,8 @@ import ma.azdad.model.PackingDetail;
 import ma.azdad.model.StockRow;
 import ma.azdad.model.StockRowStatus;
 import ma.azdad.repos.DeliveryRequestSerialNumberRepos;
+import ma.azdad.view.DeliveryRequestSerialNumberView;
+import ma.azdad.view.DeliveryRequestSerialNumberView.OutboundSerialNumberSummary;
 
 @Component
 public class DeliveryRequestSerialNumberService extends GenericService<Integer, DeliveryRequestSerialNumber, DeliveryRequestSerialNumberRepos> {
@@ -96,16 +102,52 @@ public class DeliveryRequestSerialNumberService extends GenericService<Integer, 
 	
 	///////////////// Mobile
 	public ResponseEntity<String> scanOutboundSnMobile(Integer id, String serialNumber) {
-	    DeliveryRequest dn = deliveryRequestService.findOne(id);
-	    List<DeliveryRequestSerialNumber> list = repos.findRemainingOutboundMobile(id, serialNumber);
-	    
-	    if (!list.isEmpty()) {
-	        for (DeliveryRequestSerialNumber dns : list) {
+	    DeliveryRequest dn = deliveryRequestService.findOne(id);		 
+	    DeliveryRequest deliveryRequest = dn;
+		
+		List<StockRow> list = deliveryRequest.getStockRowList().stream()
+				.filter(i -> i.getInboundDeliveryRequest().getIsSnRequired() && i.getPacking().getDetailList().stream().filter(pd -> pd.getHasSerialnumber()).count() > 0)
+				.collect(Collectors.toList());
+		List<OutboundSerialNumberSummary> outboundSerialNumberSummaryList;
+
+		outboundSerialNumberSummaryList = new ArrayList<DeliveryRequestSerialNumberView.OutboundSerialNumberSummary>();
+
+		Map<String, OutboundSerialNumberSummary> map = new HashMap<String, OutboundSerialNumberSummary>(); // key (inboundDeliveryRequestDetailId;packingDetailId)
+		for (StockRow stockRow : list) {
+			stockRow.getPacking().getDetailList().stream().filter(pd -> pd.getHasSerialnumber()).forEach(pd -> {
+				String key = stockRow.getInboundDeliveryRequestDetail().getId() + ";" + pd.getId();
+				Integer inboundDeliveryRequestDetailId = stockRow.getInboundDeliveryRequestDetail().getId();
+				String partNumberName = stockRow.getPartNumberName();
+				String inboundDeliveryRequestReference = stockRow.getInboundDeliveryRequestReference();
+				String packingName = stockRow.getPacking().getName();
+				Double usedQuantity = (double) findByDeliveryRequest(id).stream().filter(
+						i -> i.getInboundStockRow().getDeliveryRequestDetail().getId().equals(inboundDeliveryRequestDetailId) && i.getPackingDetail().getId().equals(pd.getId()))
+						.count();
+
+				map.putIfAbsent(key, new OutboundSerialNumberSummary(inboundDeliveryRequestDetailId, pd.getId(), partNumberName, inboundDeliveryRequestReference, packingName,
+						pd.getType(), usedQuantity));
+				map.get(key).setQuantity(map.get(key).getQuantity() + pd.getQuantity() * (-stockRow.getQuantity() / stockRow.getPacking().getQuantity()));
+			});
+		}
+
+		for (String key : map.keySet())
+			outboundSerialNumberSummaryList.add(map.get(key));
+		
+		List<DeliveryRequestSerialNumber> inboundList1 = new ArrayList<DeliveryRequestSerialNumber>();
+		outboundSerialNumberSummaryList.stream().filter(i -> i.getRemainingQuantity() > 0.0)
+				.forEach(i -> inboundList1.addAll(findRemainingOutbound(i.getInboundDeliveryRequestDetailId(), i.getPackingDetailId())));
+		  List<DeliveryRequestSerialNumber> matchList = inboundList1.stream()
+			        .filter(sn -> serialNumber.equals(sn.getSerialNumber()))
+			        .collect(Collectors.toList());
+	    if (!matchList.isEmpty()) {
+	        for (DeliveryRequestSerialNumber dns : matchList) {
 	            dns.setOutboundDeliveryRequest(dn);
+	            deliveryRequestService.updateMissingSerialNumber(id,
+	    				outboundSerialNumberSummaryList.stream().filter(i -> i.getRemainingQuantity() > 0).count() > 0);
 	        }
 	        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN).body("SN scanned successfully");
 	    } else {
-	        return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.TEXT_PLAIN).body("The SN does not exist");
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.TEXT_PLAIN).body("The SN does not exist or already scanned");
 	    }
 	}
 

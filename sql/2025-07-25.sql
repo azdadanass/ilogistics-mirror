584f32e83cc62816b0c1b0a8b3b037857f22cb50
Azdad Anass <azdadanass@gmail.com>
2025-07-28 20:53:16 +0100

    update 2025-07-25.sql

--- sql/2025-07-25.sql
+++ sql/2025-07-25.sql
@@ -8,3 +8,18 @@ update transporter set private_phone  = phone where private_phone is null and ph
 ALTER TABLE transporter DROP COLUMN last_name;
 ALTER TABLE transporter DROP COLUMN first_name;
 ALTER TABLE transporter DROP COLUMN phone;
+
+
+
+
+update transportation_job a set user1_username  = (select user_username from transportation_job_history tjh where tjh.parent_id = a.id and tjh.status = 'Created') ;
+update transportation_job a set date1   = (select date from transportation_job_history tjh where tjh.parent_id = a.id and tjh.status = 'Created') ;
+update transportation_job a set user2_username  = user1_username;
+update transportation_job a set date2   = date1;
+update transportation_job a set user3_username  = user1_username;
+update transportation_job a set date3   = date1;
+update transportation_job a set user4_username  = driver_username;
+update transportation_job a set date4   = date1 ;
+update transportation_job a set user5_username  = driver_username;
+update transportation_job a set date5   = date1;
+
